0a70d30b9fb4175fa213a27b80f65cd783e94233
Ryan McFarland <ryanmcfarland264@u.boisestate.edu>
2024-09-27 11:54:15 -0600

    Completed Lab M6.2

--- js/main.js
+++ js/main.js
@@ -5,14 +5,32 @@ window.onload = loaded;
  */
 function loaded() {
     // Assign to a variable so we can set a breakpoint in the debugger!
-    const hello = sayHello();
-    console.log(hello);
+    // const hello = sayHello();
+    // console.log(hello);  
+
+
+    /**
+     * Checks for a submit input, creates a selected option const, 
+     * and allows us to differentiate between the user selecting an
+     * option and not. Alerts either way.
+     */
+    optionForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        const selectedOption = document.querySelector('input[name="option"]:checked');
+        
+        if (selectedOption) {
+            alert('You selected: ' + selectedOption.value);
+        } else {
+            alert('Please select an option.');
+        }
+    });
 }
 
 /**
  * This function returns the string 'hello'
  * @return {string} the string hello
  */
-export function sayHello() {
-    return 'hello';
-}
+// export function sayHello() {
+//     return 'hello';
+// }
